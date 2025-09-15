318e7b5f8dd321632a50bf7a47f9d2a8e1279c94
alexandremaz <alexandre@mazioui.com>
2025-09-15 14:07:38 +0200

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import axios from "axios";
+import mockttp from "mockttp";
+
+const mockServer = mockttp.getLocal();
+
+await mockServer.start(8888);
+
+await mockServer.forGet("/mocked-path").thenReply(200, "A mocked response");
+
+const axiosInstance = axios.create({
+  baseURL: "https://google.com",
+  proxy: {
+    host: "localhost",
+    port: 8888,
+    protocol: "http",
+  },
+});
+
+try {
+  const response = await axiosInstance.get("/mocked-path");
+
+  console.log({
+    data: response.data,
+  });
+} catch (e) {
+  console.error(e);
+}
+
+mockServer.stop();
